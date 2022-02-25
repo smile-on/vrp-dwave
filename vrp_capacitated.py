819bb81fb80ce97dc4b761d8fa55037865a87de0
slav kochepasov <vkochepasov@gmail.com>
2022-02-25 13:57:58 -0500

    Go far model.

--- vrp_capacitated.py
+++ vrp_capacitated.py
@@ -1,5 +1,5 @@
 from dimod import ConstrainedQuadraticModel, Binary, quicksum
-from dwave.system import LeapHybridSampler
+from dwave.system import LeapHybridCQMSampler
 
 from vrp_data import VRPData
 # formulates VRP problem
@@ -17,21 +17,18 @@ class VRPCapacitated(VRPData):
         self.formulate()
         self.solve()
         if self.status == SolutionStatus.FOUND:
-            # todo read solution
-            # solution = [k for k, v in self.best_solution.items() if v == 1]
-            return VRPSolution(self.best_solution)
+            return self.decode_solution()
         return None
 
     status = SolutionStatus.UNKNOWN
     cqm = None
-    x = {}  # Boolean[van, node, stop-1]
-    best_solution = None  # best infeasible solution if model has conflicts or infeasibility
+    best_sample = None  # can be the best infeasible solution if model has conflicts or infeasibility
 
     def formulate(self):
         self.cqm = ConstrainedQuadraticModel()
-        # BinaryArray
-        self.x = [[[Binary(f'x_{v}.{n}.{s}') for s in range(1, self.n_stops)] for n in range(self.n_nodes)] for v in
-                  range(self.n_vans)]
+        # x = Boolean[van, node, stop-1]
+        x = [[[Binary(f'x_{v}.{n}.{s}') for s in range(1, self.n_stops)] for n in range(self.n_nodes)] for v in
+             range(self.n_vans)]
         # s.t.
         # each stop is visited. Disjoint One-Hot constraint
         for s in range(1, self.n_stops):
@@ -40,39 +37,58 @@ class VRPCapacitated(VRPData):
         # each node is assigned to one stop at most
         for v in range(self.n_vans):
             for n in range(self.n_nodes):
-                self.cqm.add_constraint(quicksum(self.x[v][n][s - 1] for s in range(1, self.n_stops)) <= 1,
+                self.cqm.add_constraint(quicksum(x[v][n][s - 1] for s in range(1, self.n_stops)) <= 1,
                                         label=f"van-node-one-stop-{v}.{n}")
         # todo add constraint on capacity.
         # obj
         routes = []
         for v in range(self.n_vans):
-            # departure
+            # departure leg
             n = 0
-            leg_cost = quicksum(self.cost[0][s] * self.x[v][n][s - 1] for s in range(1, self.n_stops))
+            leg_cost = quicksum(self.cost[0][s] * x[v][n][s - 1] for s in range(1, self.n_stops))
             routes.append(leg_cost)
-            # cost[s’][s”] * x[v][n-1][s’] * x[v][n][s”]
+            # move leg = cost[s’][s”] * x[v][n-1][s’] * x[v][n][s”]
             for n in range(1, self.n_nodes):
                 leg_cost = quicksum(
-                    [self.cost[s1][s2] * self.x[v][n - 1][s1 - 1] * self.x[v][n][s2 - 1]
+                    [self.cost[s1][s2] * x[v][n - 1][s1 - 1] * x[v][n][s2 - 1]
                      for s2 in range(1, self.n_stops) for s1 in range(1, self.n_stops)])
                 routes.append(leg_cost)
         # minimize
-        self.cqm.set_objective(sum(routes))
+        self.cqm.set_objective(quicksum(routes))
 
     def solve(self):
-        # fixme debug model
-        with open("../cqm.bin", "wb", buffering=0) as f:
-            f.write(self.cqm.to_file().read())
-        answer = LeapHybridSampler().sample(self.cqm)  # time_limit=60 sec num_reads=?
+        # self.model_dump_to("../self.cqm.bin")
+        sampler = LeapHybridCQMSampler()
+        answer = sampler.sample_cqm(self.cqm)  # time_limit=5 sec num_reads=?
         try:
-            self.best_solution = answer.filter(lambda d: d.is_feasible).first
+            self.best_sample = answer.filter(lambda d: d.is_feasible).first.sample
             self.status = SolutionStatus.FOUND
-            # fixme debug solution decoding
-            print(answer)
         except ValueError:
             try:
-                self.best_solution = answer.first
+                self.best_sample = answer.first.sample
                 self.status = SolutionStatus.INFEASIBLE
             except ValueError:
-                self.best_solution = None
+                self.best_sample = None
                 self.status = SolutionStatus.UNKNOWN
+
+    def decode_solution(self) -> VRPSolution:
+        """Interpret the sample found in terms of routes."""
+        routes = []
+        for v in range(self.n_vans):
+            route = [0]
+            for n in range(self.n_nodes):
+                for s in range(1, self.n_stops):
+                    # best_sample.sample['x']
+                    if self.best_sample[f'x_{v}.{n}.{s}'] > 0:
+                        route.append(s)
+                        break
+                pass
+            pass
+            routes.append(route)
+        pass
+        return VRPSolution(routes, self.cost)
+
+    def model_dump_to(self, file_name: str):
+        """Write dump of the cqm model into the file for debugging purpose."""
+        with open(file_name, "wb", buffering=0) as f:
+            f.write(self.cqm.to_file().read())
--- vrp_solution.py
+++ vrp_solution.py
@@ -11,8 +11,19 @@ class SolutionStatus(Enum):
 
 class VRPSolution:
 
-    def __init__(self, sample):
-        self.sample = sample
+    def __init__(self, routes, cost):
+        """Interpret the sample found in terms of routes
+        Args:
+            routes [[0, 3, 1], [0, 2]] """""
+        self.routes = routes
+        self.total_cost = 0
+        for r in routes:
+            s1 = None
+            for s2 in r:
+                if s1 is not None:
+                    self.total_cost += cost[s1][s2]
+                s1 = s2
 
     def str(self) -> str:
-        return self.sample.str()
+        """describe the solution"""
+        return f' routes {self.routes.__str__()} \n total cost {self.total_cost}'
